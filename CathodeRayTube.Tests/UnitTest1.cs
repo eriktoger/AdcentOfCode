082965000ba54d49425fb48617f32caecafb6a1c
Erik Töger <erik.toger@gmail.com>
2024-10-23 19:21:32 +0200

    AoC 2022 Day 10: Cathode-Ray Tube, part 2

--- CathodeRayTube.Tests/UnitTest1.cs
+++ CathodeRayTube.Tests/UnitTest1.cs
@@ -7,7 +7,7 @@ public class UnitTest1
     public void Example()
     {
         string fullPath = Path.GetFullPath("example.txt");
-        var answer = Solution.PartOne(fullPath);
+        var answer = Solution.Solve(fullPath);
         Assert.Equal(13140, answer);
     }
 
@@ -15,7 +15,46 @@ public class UnitTest1
     public void PartOne()
     {
         string fullPath = Path.GetFullPath("part1.txt");
-        var answer = Solution.PartOne(fullPath);
+        var answer = Solution.Solve(fullPath);
         Assert.Equal(17020, answer);
+    }
+
+
+    private static void CompareFiles(string inputFile, string outputFile, string solutionFile)
+    {
+
+        File.WriteAllText(outputFile, string.Empty);
+        Solution.Solve(inputFile, outputFile);
+
+        var actualLines = File.ReadAllLines(outputFile);
+        var expectedLines = File.ReadAllLines(solutionFile);
+
+
+        Assert.Equal(expectedLines.Length, actualLines.Length);
+
+        for (int i = 0; i < expectedLines.Length; i++)
+        {
+            Assert.Equal(expectedLines[i], actualLines[i]);
+        }
+    }
+
+
+    [Fact]
+    public void PartTwoExample()
+    {
+        string inputPath = Path.GetFullPath("example.txt");
+        string outputPath = Path.GetFullPath("outputExample.txt");
+        string solutionPath = Path.GetFullPath("outputExampleSolution.txt");
+        CompareFiles(inputPath, outputPath, solutionPath);
+
+    }
+
+    [Fact]
+    public void PartTwo()
+    {
+        string inputPath = Path.GetFullPath("part1.txt");
+        string outputPath = Path.GetFullPath("output.txt");
+        string solutionPath = Path.GetFullPath("outputSolution.txt");
+        CompareFiles(inputPath, outputPath, solutionPath);
     }
 }
--- CathodeRayTube/Program.cs
+++ CathodeRayTube/Program.cs
@@ -2,18 +2,46 @@
 namespace CathodeRayTube;
 public static class Solution
 {
-    public static int PartOne(string filePath)
+
+    private static void Draw(int xRegister, int cycleCounter, string outputFilePath)
+    {
+        using StreamWriter writer = new(outputFilePath, append: true);
+        var xPosition = cycleCounter % 40;
+
+        if (xPosition == 0 && cycleCounter != 0)
+        {
+            writer.WriteLine("");
+        }
+
+        var hitsThreeSprites = xPosition == xRegister - 1 || xPosition == xRegister || xPosition == xRegister + 1;
+        if (hitsThreeSprites)
+        {
+            writer.Write("#");
+        }
+        else
+        {
+            writer.Write(".");
+        }
+
+    }
+    public static int Solve(string filePath, string outputFilePath = "")
     {
         int sum = 0;
         int xRegister = 1;
         int[] cycleCheckPoints = [20, 60, 100, 140, 180, 220];
         int cycleCounter = 0;
 
+
         using (StreamReader reader = new(filePath))
         {
             string line;
             while ((line = reader.ReadLine()!) != null)
             {
+
+                if (outputFilePath != "")
+                {
+                    Draw(xRegister, cycleCounter, outputFilePath);
+                }
                 cycleCounter++;
                 if (cycleCheckPoints.Contains(cycleCounter))
                 {
@@ -22,7 +50,13 @@ public static class Solution
 
                 if (line.StartsWith("addx "))
                 {
+
+                    if (outputFilePath != "")
+                    {
+                        Draw(xRegister, cycleCounter, outputFilePath);
+                    }
                     cycleCounter++;
+
                     if (cycleCheckPoints.Contains(cycleCounter))
                     {
                         sum += xRegister * cycleCounter;
@@ -33,7 +67,6 @@ public static class Solution
 
                     xRegister += number;
                 }
-
             }
         }
         return sum;
